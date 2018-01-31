8cd40f1baf263832c5430c9eff7b32b3e726757c
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-01-31 15:29:31 +0300

    Add bts-ce-api submodule

--- api/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-bind = "0.0.0.0:8181"
-workers = 4
-limit_request_line = 0
-timeout=300
--- api/run.py
+++
@@ -1,4 +0,0 @@
-from btsapi import app
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8181, debug=True)
