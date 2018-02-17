6f2bba9ab2cbf196cd0f3d3d033670d8610c36d5
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-02-17 23:57:08 +0300

    Add tasks to process Huawei CM data to cm_etlp dag

--- mediation/dags/cm_etlp.py
+++ mediation/dags/cm_etlp.py
@@ -313,20 +313,25 @@ def is_vendor_supported(vendor_id):
 def is_ericsson_supported():
     if is_vendor_supported(1) is True:
         return 'ericsson_is_supported'
-    return 'end_cm_etlp'
+    return 'eri_not_supported'
 
 
 def is_huawei_supported():
     if is_vendor_supported(2) is True:
         return 'huawei_is_supported'
-    return 'end_cm_etlp'
+    return 'huawei_not_supported'
 
 
 def is_zte_supported():
-    is_vendor_supported(3)
+    if is_vendor_supported(3) is True:
+        return 'zte_is_supported'
+    return 'zte_not_supported'
+
 
 def is_nokia_supported():
-    is_vendor_supported(4)
+    if is_vendor_supported(4) is True:
+        return 'nokia_is_supported'
+    return 'nokia_not_supported'
 
 t23 = BranchPythonOperator(
     task_id='is_ericsson_supported',
@@ -337,18 +342,18 @@ t23 = BranchPythonOperator(
 # Dummy start task
 t24 = DummyOperator(task_id='start_cm_etlp', dag=dag)
 
-t25 = PythonOperator(
+t25 = BranchPythonOperator(
     task_id='is_huawei_supported',
     python_callable=is_huawei_supported,
     dag=dag)
 
-t26 = PythonOperator(
+t26 = BranchPythonOperator(
     task_id='is_zte_supported',
     python_callable=is_zte_supported,
     dag=dag)
 
 
-t27 = PythonOperator(
+t27 = BranchPythonOperator(
     task_id='is_nokia_supported',
     python_callable=is_nokia_supported,
     dag=dag)
@@ -391,11 +396,88 @@ t34 = DummyOperator(task_id='ericsson_is_supported', dag=dag)
 
 t35 = DummyOperator(task_id='huawei_is_supported', dag=dag)
 
+t36 = DummyOperator(task_id='eri_not_supported', dag=dag)
+
+t37 = DummyOperator(task_id='huawei_not_supported', dag=dag)
+
+t38 = DummyOperator(task_id='zte_is_supported', dag=dag)
+
+t39 = DummyOperator(task_id='zte_not_supported', dag=dag)
+
+t40 = DummyOperator(task_id='nokia_is_supported', dag=dag)
+
+t41 = DummyOperator(task_id='nokia_not_supported', dag=dag)
+
+t42 = BashOperator(
+    task_id='check_if_hua_3g_raw_files_exist',
+    bash_command='if [ 0 -eq `ls -1 /mediation/data/cm/huawei/3g/raw/in | wc -l` ]; then exit 1; fi',
+    dag=dag)
+
+
+t43 = BashOperator(
+    task_id='backup_huawei_3g_csv_files',
+    bash_command='mv -f /mediation/data/cm/huawei/3g/parsed/in/* /mediation/data/cm/huawei/3g/parsed/out/ 2>/dev/null',
+    dag=dag)
+
+t44 = BashOperator(
+    task_id='run_huawei_3g_parser',
+    bash_command='java -jar /mediation/bin/boda-huaweinbixmlparser.jar /mediation/data/cm/huawei/3g/raw/in /mediation/data/cm/huawei/3g/parsed/in /mediation/conf/cm/hua_cm_3g_nbi_parameters.cfg',
+    dag=dag)
+
+# Clear 3G CM data tables
+def clear_huawei_3g_cm_tables():
+    pass
+
+
+t45 = PythonOperator(
+    task_id='clear_huawei_3g_cm_tables',
+    python_callable=clear_huawei_3g_cm_tables,
+    dag=dag)
+
+
+t46 = BashOperator(
+    task_id='import_huawei_3g_cm_data',
+    bash_command='export PGPASSWORD=password && psql -h $POSTGRES_HOST -U bodastage -d bts -a -w -f "/mediation/conf/cm/hua_cm_3g_nbi_loader.cfg"',
+    dag=dag)
+
+t47 = BashOperator(
+    task_id='check_if_hua_4g_raw_files_exist',
+    bash_command='if [ 0 -eq `ls -1 /mediation/data/cm/huawei/4g/raw/in | wc -l` ]; then exit 1; fi',
+    dag=dag)
+
+t48 = BashOperator(
+    task_id='backup_huawei_4g_csv_files',
+    bash_command='mv -f /mediation/data/cm/huawei/4g/parsed/in/* /mediation/data/cm/huawei/4g/parsed/out/ 2>/dev/null',
+    dag=dag)
+
+
+t49 = BashOperator(
+    task_id='run_huawei_4g_parser',
+    bash_command='java -jar /mediation/bin/boda-huaweinbixmlparser.jar /mediation/data/cm/huawei/4g/raw/in /mediation/data/cm/huawei/4g/parsed/in /mediation/conf/cm/hua_cm_4g_nbi_parameters.cfg',
+    dag=dag)
+
+# Clear 4G CM data tables
+def clear_huawei_4g_cm_tables():
+    pass
+
+
+t50 = PythonOperator(
+    task_id='clear_huawei_4g_cm_tables',
+    python_callable=clear_huawei_4g_cm_tables,
+    dag=dag)
+
+t51 = BashOperator(
+    task_id='import_huawei_4g_cm_data',
+    bash_command='export PGPASSWORD=password && psql -h $POSTGRES_HOST -U bodastage -d bts -a -w -f "/mediation/conf/cm/hua_cm_4g_nbi_loader.cfg"',
+    dag=dag)
+
+
 # extract_ericsson_3g_sites
 # Build dependency graph
 dag.set_dependency('start_cm_etlp','is_ericsson_supported')
 dag.set_dependency('is_ericsson_supported','ericsson_is_supported')
-dag.set_dependency('is_ericsson_supported','end_cm_etlp')
+dag.set_dependency('is_ericsson_supported','eri_not_supported')
+dag.set_dependency('eri_not_supported','end_cm_etlp')
 
 dag.set_dependency('ericsson_is_supported','check_if_eri_3g4g_raw_files_exist')
 dag.set_dependency('check_if_eri_3g4g_raw_files_exist','backup_prev_eri_3g4g_csv_files')
@@ -445,12 +527,15 @@ dag.set_dependency('extract_ericsson_3g_cells','build_network_tree')
 dag.set_dependency('extract_ericsson_4g_cells','build_network_tree')
 dag.set_dependency('build_network_tree','end_cm_etlp')
 
-# Huawei 2G
-# ##############################################
-dag.set_dependency('is_huawei_supported','huawei_is_supported')
-dag.set_dependency('is_huawei_supported','end_cm_etlp')
-
+# ###########################################################################
+# Huawei
+# ##########################################################################
 dag.set_dependency('start_cm_etlp','is_huawei_supported')
+dag.set_dependency('is_huawei_supported','huawei_is_supported')
+dag.set_dependency('is_huawei_supported','huawei_not_supported')
+dag.set_dependency('huawei_not_supported','end_cm_etlp')
+
+# Huawei 2G
 dag.set_dependency('huawei_is_supported','check_if_hua_2g_raw_files_exist')
 dag.set_dependency('check_if_hua_2g_raw_files_exist','backup_huawei_2g_csv_files')
 dag.set_dependency('backup_huawei_2g_csv_files','run_huawei_2g_parser')
@@ -458,11 +543,34 @@ dag.set_dependency('run_huawei_2g_parser','clear_huawei_2g_cm_tables')
 dag.set_dependency('clear_huawei_2g_cm_tables','import_huawei_2g_cm_data')
 dag.set_dependency('import_huawei_2g_cm_data','end_cm_etlp')
 
+# Huawei 3G
+dag.set_dependency('huawei_is_supported','check_if_hua_3g_raw_files_exist')
+dag.set_dependency('check_if_hua_3g_raw_files_exist','backup_huawei_3g_csv_files')
+dag.set_dependency('backup_huawei_3g_csv_files','run_huawei_3g_parser')
+dag.set_dependency('run_huawei_3g_parser','clear_huawei_3g_cm_tables')
+dag.set_dependency('clear_huawei_3g_cm_tables','import_huawei_3g_cm_data')
+dag.set_dependency('import_huawei_3g_cm_data','end_cm_etlp')
+
+# Huawei 4G
+dag.set_dependency('huawei_is_supported','check_if_hua_4g_raw_files_exist')
+dag.set_dependency('check_if_hua_4g_raw_files_exist','backup_huawei_4g_csv_files')
+dag.set_dependency('backup_huawei_4g_csv_files','run_huawei_4g_parser')
+dag.set_dependency('run_huawei_4g_parser','clear_huawei_4g_cm_tables')
+dag.set_dependency('clear_huawei_4g_cm_tables','import_huawei_4g_cm_data')
+dag.set_dependency('import_huawei_4g_cm_data','end_cm_etlp')
+
 # ZTE
 # ##############################################
 dag.set_dependency('start_cm_etlp','is_zte_supported')
-dag.set_dependency('is_zte_supported','end_cm_etlp')
+dag.set_dependency('is_zte_supported','zte_is_supported')
+dag.set_dependency('is_zte_supported','zte_not_supported')
+dag.set_dependency('zte_not_supported','end_cm_etlp')
+dag.set_dependency('zte_is_supported','end_cm_etlp')
+
 # Nokia
 # ##############################################
 dag.set_dependency('start_cm_etlp','is_nokia_supported')
-dag.set_dependency('is_nokia_supported','end_cm_etlp')
+dag.set_dependency('is_nokia_supported','nokia_is_supported')
+dag.set_dependency('is_nokia_supported','nokia_not_supported')
+dag.set_dependency('nokia_not_supported','end_cm_etlp')
+dag.set_dependency('nokia_is_supported','end_cm_etlp')
