d6e6b2ef5e64f6e455ec54f4d2c8351217cf225d
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-02-26 15:33:04 +0300

    Extract Huawei 3G-3G nbrs for inter and intra frequency relations

--- mediation/packages/bts/process_cm_data.py
+++ mediation/packages/bts/process_cm_data.py
@@ -1554,6 +1554,7 @@ class ProcessCMData(object):
             self.extract_huawei_3g3g_interfreq_nbrs_internal()
             self.extract_huawei_3g3g_interfreq_nbrs_external()
 
+            self.extract_huawei_3g3g_intrafreq_nbrs_with_all_vendors()
             # self.extract_huawei_3g3g_intrafreq_nbrs_with_ericsson()
             # self.extract_huawei_3g3g_interfreq_nbrs_with_ericsson()
 
@@ -1867,6 +1868,69 @@ class ProcessCMData(object):
                     t5.pk = t4.site_pk 
                     AND t5.vendor_pk = 2 AND t5.tech_pk = 2
                 -- -----------
+                LEFT JOIN hua_cm_3g.uext3gcell t8 on 
+                    t8.neid = t1.neid
+                    AND t8."CELLID" = t1."NCELLID"
+                LEFT JOIN live_network.cells t6 ON 
+                    t6.name = t8."CELLNAME"
+                LEFT JOIN live_network.sites t7 ON 
+                    t7.pk = t6.site_pk 
+                WHERE 
+                 t4.site_pk = '{0}'
+            """.format(site_pk)
+
+            self.db_engine.execute(text(sql).execution_options(autocommit=True))
+
+        session.close()
+
+    def extract_huawei_3g3g_interfreq_nbrs_with_all_vendors(self):
+        """
+        Extract  Huawei 3G- Huawei 3G neighbour relations on same RNC with diffrent frequencies
+        """
+        Session = sessionmaker(bind=self.db_engine)
+        session = Session()
+
+        metadata = MetaData()
+        Site = Table('sites', metadata, autoload=True, autoload_with=self.db_engine, schema="live_network")
+        for site in session.query(Site).filter_by(vendor_pk=2).filter_by(tech_pk=2).yield_per(5):
+            (site_pk, site_name) = (site[0], site[1])
+
+            print(
+            "Extracting Huawei 3G- Ericsson 3G relations for site_pk: {0}, site_name: {1}".format(site_pk, site_name))
+
+            sql = """
+                INSERT INTO live_network.relations 
+                (pk, svrnode_pk,svrsite_pk, svrtech_pk, svrvendor_pk, svrcell_pk,nbrnode_pk,nbrsite_pk,nbrtech_pk, nbrvendor_pk,nbrcell_pk,date_added,date_modified, added_by, modified_by)
+                SELECT 
+                NEXTVAL('live_network.seq_relations_pk'),
+                -- serving side
+                t5.node_pk as svrnode_pk,
+                t4.site_pk as svrsite_pk,
+                t4.tech_pk as svrtech_pk,
+                t4.vendor_pk as svrvendor_pk,
+                t4.pk as svrcell_pk,
+                -- nbr side
+                t7.node_pk as nbrnode_pk,
+                t6.site_pk as nbrsite_pk,
+                t6.tech_pk as nbrtech_pk,
+                t6.vendor_pk as nbrvendor_pk,
+                t6.pk as svrcell_pk,
+                t1."varDateTime" ,
+                t1."varDateTime" ,
+                0, -- system
+                0
+                FROM 
+                hua_cm_3g.uinterfreqncell t1
+                INNER JOIN hua_cm_3g.UCELL t2 on 
+                    t2.neid  = t1.neid 
+                    AND t1."CELLID" = t2."CELLID"
+                INNER JOIN live_network.cells t4 ON 
+                    t4.name = t2."CELLNAME" 
+                    AND t4.vendor_pk = 2 AND t4.tech_pk = 2
+                INNER JOIN live_network.sites t5 ON 
+                    t5.pk = t4.site_pk 
+                    AND t5.vendor_pk = 2 AND t5.tech_pk = 2
+                -- ---------
                 LEFT JOIN hua_cm_3g.uext3gcell t8 on 
                     t8.neid = t1.neid
                     AND t8."CELLID" = t1."NCELLID"
