2559f52431e274ae0c6c564f6689e0076b4a337d
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-12-14 14:56:17 +0300

    Run insert queries

--- mediation/bin/run_cm_load_insert_queries.py
+++ mediation/bin/run_cm_load_insert_queries.py
@@ -11,19 +11,19 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
 
 if len(sys.argv) != 2: 
-	print("Format: {0} {1} {2}".format( os.path.basename(__file__)))
-	sys.exit()
+    print("Format: {0} {1} {2}".format( os.path.basename(__file__)))
+    sys.exit()
 
 file_format=sys.argv[1]
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
-							os.getenv("BTS_DB_USER", "bodastage"),
-							os.getenv("BTS_DB_PASS", "password"),
-							os.getenv("BTS_DB_HOST", "192.168.99.100"),
-							os.getenv("BTS_DB_PORT", "5432"),
-							os.getenv("BTS_DB_NAME", "bts"),
-						)
-						
+                            os.getenv("BTS_DB_USER", "bodastage"),
+                            os.getenv("BTS_DB_PASS", "password"),
+                            os.getenv("BTS_DB_HOST", "192.168.99.100"),
+                            os.getenv("BTS_DB_PORT", "5432"),
+                            os.getenv("BTS_DB_NAME", "bts"),
+                        )
+
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -38,9 +38,13 @@ sql = "SELECT insert_query FROM cm_load_insert_queries WHERE file_format = '{}' 
 result = engine.execute(sql)
 
 for lq in  result.fetchall():
-    query = "{} {}".format( lq[0],";")
-    print("Inserting {} data...".format(file_format))
-    print("{}".format(query))
-    engine.execute(query)
+    try:
+        query = "{} {}".format( lq[0],";")
+        print("Inserting {} data...".format(file_format))
+        print("{}".format(query))
+        engine.execute(query)
+    except Exception as err:
+        print("Error: {}".format(err))
+
 
     # subprocess.call(["psql", "-U", "bodastage", "-d", "bts","-h","database" ,"-c", query], env=loader_env)
