2c368b5fb8140f2a3f8c1ab5943f04c69af59d3c
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-02-26 00:40:04 +0300

    Create default msg broker user

--- queue_scripts/queue_setup.sh
+++ queue_scripts/queue_setup.sh
@@ -1,13 +1,9 @@
-#rabbitmqctl add_user btsuser Password@7
-#rabbitmqctl set_user_tags btsuser administrator
-#rabbitmqctl add_vhost /bs
-#rabbitmqctl set_permissions -p /bs btsuser ".*" ".*" ".*"
-#rabbitmqctl delete_user guest
+#!/bin/bash
 
 # Enable stomp
 rabbitmq-plugins enable rabbitmq_web_stomp
 
-
 # Re-start service
 rabbitmq-server stop
 rabbitmq-server start
+
